66b4342d23c014323968e2c274bd7abcdd99b84e
Michael <msiller455@gmail.com>
2019-01-14 15:12:33 -0800

    Tip calculator express application created

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ app.get('/greeting/:name', (req, res) => {
     res.send(`Hello ${req.params.name}!`);
 });
 
-
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    res.send(`${req.params.total * req.params.tipPercentage / 100}`);
+});
 
 
 app.listen(8000, () => {
